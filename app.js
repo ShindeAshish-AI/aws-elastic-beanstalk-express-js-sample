2d16ba0c37865922c8d3f83388cd5632571ee120
ShindeAshish-AI <68138472+ShindeAshish-AI@users.noreply.github.com>
2021-08-24 07:39:12 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Life is Wonderful!'));
+app.get('/', (req, res) => res.send('Life is Wonderful & Ise laga dala to Life zinga la la!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
